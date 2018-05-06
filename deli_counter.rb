d565130e02e882d63b00be4c90812749ca7731a2
Anthony Lam <al3101@nyu.edu>
2018-05-06 17:06:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,10 @@ def take_a_number(katz_deli, name)
 end 
 
 def now_serving(katz_deli)
-  person = katz_deli[0] 
-  katz_deli.shift() 
-  puts "Currently serving #{person}."
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  else 
+    person = katz_deli[0] 
+    katz_deli.shift() 
+    puts "Currently serving #{person}."
 end 
