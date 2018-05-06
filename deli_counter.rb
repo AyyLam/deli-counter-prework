7b0aa2cd4c8d1a4d4e2de8bff08bbccaf4568fbe
Anthony Lam <al3101@nyu.edu>
2018-05-06 17:05:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,3 +18,9 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name) 
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end 
+
+def now_serving(katz_deli)
+  person = katz_deli[0] 
+  katz_deli.shift() 
+  puts "Currently serving #{person}."
+end 
